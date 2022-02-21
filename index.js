7cec2131b060e3aa5fb5b3165b3a3714589c9080
dianabayazitova <d651332@yandex.ru>
2022-02-21 16:15:37 +0300

    lab 1-4

---
+++ index.js
@@ -0,0 +1,12 @@
+// Activate selected tab in navigation bar
+const href = document.location.href.split('/');
+const pageName = href[href.length - 1].split('.')[0];
+document.getElementById(pageName).classList.add('active');
+
+document.getElementById('page-loading').innerHTML =
+(function onLoad() {
+    const now = new Date().getTime();
+    const pageLoadTime = now - performance.timing.navigationStart;
+    console.log('Page loading time: ' + pageLoadTime);
+    return 'Page loading time: ' + pageLoadTime / 1000 + 's';
+})();
